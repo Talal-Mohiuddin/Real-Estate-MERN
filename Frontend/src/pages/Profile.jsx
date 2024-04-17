9bd96dcc183b6e67aedbe0412adf4606f129da3b
Talal Mohiuddin <hafiztalal8722@gmail.com>
2024-04-17 21:52:09 +0500

    Update Profile component to display user profile information and allow for updating user details

--- Frontend/src/pages/Profile.jsx
+++ Frontend/src/pages/Profile.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import React from "react";
+import { useSelector } from "react-redux";
 
 const Profile = () => {
+  const { user } = useSelector((state) => state.user);
+  const userProfile = user?.user?.avatar;
   return (
-    <div>Profile</div>
-  )
-}
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
+      <form className="flex gap-4 flex-col">
+        <img
+          src={userProfile}
+          className="rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2"
+        />
+        <input
+          type="text"
+          placeholder="Name"
+          className="border p-3 rounded-lg my-3"
+          id="name"
+        />
+        <input
+          type="email"
+          placeholder="Email"
+          className="border p-3 rounded-lg my-3"
+          id="email"
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          className="border p-3 rounded-lg my-3"
+          id="password"
+        />
+        <button className="bg-slate-700 p-3 uppercase text-white rounded-lg hover:opacity-95 disabled:opacity-80 ">
+          update
+        </button>
+      </form>
+      <div className="flex justify-between mt-5">
+        <span className="text-red-700 cursor-pointer">Delete Account</span>
+        <span className="text-red-700 cursor-pointer">Sign Out</span>
+      </div>
+    </div>
+  );
+};
 
-export default Profile
+export default Profile;
